0419628b66cbcc080c341af802310edfde425e8a
Paul Watson <thisoldcoder@gmail.com>
2022-03-02 02:17:38 -0700

    Added function to test TargetServer.

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,12 @@
 import { NS } from '@ns'
 import { Scanner } from '/scripts/scanner'
+import { TargetServer } from '/target_server';
 
 export async function main(ns : NS) : Promise<void> {
     if (ns.args.find(element => element === 'scanner'))
         testScanner(ns);
+    if (ns.args.find(element => element === 'target_server'))
+        testTargetServer(ns);
 }
 
 function testScanner(ns: NS): void {
@@ -11,4 +14,9 @@ function testScanner(ns: NS): void {
     scanner.scanServers();
     for(const server of scanner.serverList)
         ns.tprintf(server.targetName + ": " + server.rating )
+}
+
+function testTargetServer(ns: NS): void {
+    const target = new TargetServer(ns, 'the-hub');
+    ns.tprintf('Threads to weaken: ' + target.threadCountToWeaken());
 }
