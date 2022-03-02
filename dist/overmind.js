9f21b277876e568bfe0e3e27c1919642a6a6dbf5
Paul Watson <thisoldcoder@gmail.com>
2022-03-02 02:38:41 -0700

    Initial overmind.

--- dist/overmind.js
+++ dist/overmind.js
@@ -1,4 +1,16 @@
-export async function main(ns) {
-    ns.tprint('foo');
+import { Scanner } from '/scanner';
+class Overmind {
+    ns; // Netscript handle
+    scanner; // Scanner to find new hacking targets, holds list
+    constructor(ns) {
+        this.ns = ns;
+        this.scanner = new Scanner(this.ns);
+    }
+    start() {
+        this.ns.tprintf("Starting with " + this.scanner.serverList.length + " viable targets.");
+    }
 }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoib3Zlcm1pbmQuanMiLCJzb3VyY2VSb290IjoiaHR0cDovL2xvY2FsaG9zdDo4MDAwL3NvdXJjZXMvIiwic291cmNlcyI6WyJvdmVybWluZC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFHQSxNQUFNLENBQUMsS0FBSyxVQUFVLElBQUksQ0FBQyxFQUFNO0lBQ2hDLEVBQUUsQ0FBQyxNQUFNLENBQUMsS0FBSyxDQUFDLENBQUM7QUFDbEIsQ0FBQyJ9
+export async function main(ns) {
+    new Overmind(ns).start();
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoib3Zlcm1pbmQuanMiLCJzb3VyY2VSb290IjoiaHR0cDovL2xvY2FsaG9zdDo4MDAwL3NvdXJjZXMvIiwic291cmNlcyI6WyJvdmVybWluZC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFDQSxPQUFPLEVBQUUsT0FBTyxFQUFFLE1BQU0sVUFBVSxDQUFDO0FBR25DLE1BQU0sUUFBUTtJQUNWLEVBQUUsQ0FBQyxDQUFXLG1CQUFtQjtJQUNwQyxPQUFPLENBQUMsQ0FBRyxrREFBa0Q7SUFFN0QsWUFBWSxFQUFNO1FBQ2pCLElBQUksQ0FBQyxFQUFFLEdBQUcsRUFBRSxDQUFDO1FBQ2IsSUFBSSxDQUFDLE9BQU8sR0FBRyxJQUFJLE9BQU8sQ0FBQyxJQUFJLENBQUMsRUFBRSxDQUFDLENBQUM7SUFDckMsQ0FBQztJQUVELEtBQUs7UUFDSixJQUFJLENBQUMsRUFBRSxDQUFDLE9BQU8sQ0FBQyxnQkFBZ0IsR0FBRyxJQUFJLENBQUMsT0FBTyxDQUFDLFVBQVUsQ0FBQyxNQUFNLEdBQUcsa0JBQWtCLENBQUMsQ0FBQztJQUN6RixDQUFDO0NBQ0Q7QUFFRCxNQUFNLENBQUMsS0FBSyxVQUFVLElBQUksQ0FBQyxFQUFNO0lBQ2hDLElBQUksUUFBUSxDQUFDLEVBQUUsQ0FBQyxDQUFDLEtBQUssRUFBRSxDQUFDO0FBQzFCLENBQUMifQ==
--- src/overmind.ts
+++ src/overmind.ts
@@ -1,6 +1,21 @@
 import { NS, ProcessInfo, Server } from '@ns'
-import { TargetServer } from '/target_server'
+import { Scanner } from '/scanner';
+import { TargetServer } from '/scripts/target_server'
 
-export async function main(ns: NS) {
-	ns.tprint('foo');
+class Overmind {
+    ns;			        // Netscript handle
+	scanner;			// Scanner to find new hacking targets, holds list
+	
+	constructor(ns: NS) {
+		this.ns = ns;
+		this.scanner = new Scanner(this.ns);
+	}
+
+	start(): void {
+		this.ns.tprintf("Starting with " + this.scanner.serverList.length + " viable targets.");
+	}
+}
+
+export async function main(ns: NS): Promise<void> {
+	new Overmind(ns).start();
 }
